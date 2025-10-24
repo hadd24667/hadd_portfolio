df344e595dfbb9670453ef0dfd9b919ae53040cb
hadd24667 <hahnj24667@gmail.com>
2025-10-24 17:45:34 +0700

    update resume

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -220,7 +220,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1WmDStWMYjBn9VJOIZeuodOvJFr2_1HzF/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1VyPPIzmqxV13Oac80EiNX5UK_RAntYEK/view?usp=sharing" className="w-full lg:w-auto">
               <button 
                 data-aos="fade-up"
                 data-aos-duration="800"
